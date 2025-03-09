baab0db051e0d10f9d5f02f4a1c56687b7abbb01
lubnanargees <nargeeslubna@gmail.com>
2025-03-09 14:53:52 +0530

    Add files via upload
    Python offers powerful data structures like lists, dictionaries, sets, and tuples for efficient data management. Lists allow dynamic modifications, dictionaries store key-value pairs, sets handle unique elements, and tuples provide immutable sequences. These exercises cover adding, modifying, and retrieving data to enhance problem-solving skills using Python’s built-in data structures.

---
+++ Assignment2.py
@@ -0,0 +1,29 @@
+print("Exercise 11: List Operations")
+my_list = [4, 7, 1, 9, 3]
+print("Original list:", my_list)
+my_list.extend([10, 15, 20])
+print("Updated list:", my_list)
+for num in my_list:
+    print(num)
+print("\n")
+
+print("Exercise 12: Dictionary Operations")
+my_dict = {"name": "John", "age": 25, "address": "New York"}
+print("Original dictionary:", my_dict)
+my_dict["phone"] = "1234567890"
+print("Updated dictionary:", my_dict)
+print("\n")
+
+print(" Exercise 13: Set Operations")
+my_set = {1, 2, 3, 4, 5}
+print("Original set:", my_set)
+my_set.add(6)
+print("Updated set:", my_set)
+my_set.remove(3)
+print("Set after removal:", my_set)
+print("\n")
+
+print("Exercise 14: Tuple Operations")
+my_tuple = (1, 2, 3, 4)
+print("Tuple:", my_tuple)
+print("Length of tuple:", len(my_tuple))
